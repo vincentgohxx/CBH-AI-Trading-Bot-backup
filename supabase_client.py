08e7287b715643578084dba63d7dfe70c98f6b26
vincentgohxx <52526227+vincentgohxx@users.noreply.github.com>
2025-07-23 04:27:57 +0800

    Update supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -5,3 +5,7 @@ SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+# 获取所有 users 数据
+res = supabase.table("users").select("*").execute()
+print(res)
